b78ac13e2eb20f56526f71559dc1788a08ae8c6b
yead0090 <yeadhossain2004@gmail.com>
2024-09-25 22:12:11 +0600

    3rd commit

---
+++ functions.js
@@ -0,0 +1,102 @@
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+const donTabBtn = document.getElementById("donate-tab-btn");
+const histTabBtn = document.getElementById("history-tab-btn");
+const donTab = document.getElementById("donation-tab");
+const histTab = document.getElementById("history-tab");
+
+
+
+
+
+
+
+btn1.addEventListener("click", function () {
+    const input1 = Number(document.getElementById("input-1").value);
+    let result1 = Number(document.getElementById("result-1").innerText);
+    const balance = Number(document.getElementById("balance").innerText);
+    document.getElementById("result-1").innerText = result1 + input1;
+    let newBalance = balance - input1;
+    document.getElementById("balance").innerText = newBalance;
+    return;
+})
+btn2.addEventListener("click", function () {
+    const input2 = Number(document.getElementById("input-2").value);
+    let result2 = Number(document.getElementById("result-2").innerText);
+    const balance = Number(document.getElementById("balance").innerText);
+    document.getElementById("result-2").innerText = result2 + input2;
+    let newBalance = balance - input2;
+    document.getElementById("balance").innerText = newBalance;
+    return;
+})
+btn3.addEventListener("click", function () {
+    const input3 = Number(document.getElementById("input-3").value);
+    let result3 = Number(document.getElementById("result-3").innerText);
+    const balance = Number(document.getElementById("balance").innerText);
+    document.getElementById("result-3").innerText = result3 + input3;
+    let newBalance = balance - input3;
+    document.getElementById("balance").innerText = newBalance;
+    return;
+})
+
+histTabBtn.addEventListener("click", function () {
+    const histTab = document.getElementById("history-tab");
+    donTab.classList.add("hidden");
+    histTab.classList.remove("hidden");
+    donTabBtn.classList.remove("bg-[#B4F461]");
+    histTabBtn.classList.add("bg-[#B4F461]");
+
+
+
+})
+
+
+
+
+
+
+donTabBtn.addEventListener("click", function () {
+    const donTab = document.getElementById("donation-tab");
+    donTab.classList.remove("hidden")
+    donTabBtn.classList.add("bg-[#B4F461]");
+    histTabBtn.classList.remove("bg-[#B4F461]")
+
+
+})
+
+
+btn1.addEventListener("click", function () {
+    const input1 = Number(document.getElementById("input-1").value);
+    const histTab = document.getElementById("history-tab");
+    const histDiv = document.createElement("div")
+    histDiv.innerHTML=
+        `<p class="font-extrabold" >${input1}Taka is Donated for Flood at Noakhali, Bangladesh</p>
+         <p>Date:${new Date().toString()}</p>`;
+    
+    histTab.insertBefore(histDiv , histTab.firstChild);
+})
+
+
+btn2.addEventListener("click", function () {
+    const input2 = Number(document.getElementById("input-2").value);
+    const histTab = document.getElementById("history-tab");
+    const histDiv = document.createElement("div")
+    histDiv.innerHTML=
+        `<p class="font-extrabold" >${input2}Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+         <p>Date:${new Date().toString()}</p>`;
+    
+    histTab.insertBefore(histDiv , histTab.firstChild);
+})
+
+
+btn3.addEventListener("click", function () {
+    const input3 = Number(document.getElementById("input-3").value);
+    const histTab = document.getElementById("history-tab");
+    const histDiv = document.createElement("div")
+    histDiv.innerHTML=
+        `<p class="font-extrabold" >${input3}Taka is Aid for Injured in the Quota Movement</p>
+         <p>Date:${new Date().toString()}</p>`;
+    
+    histTab.insertBefore(histDiv , histTab.firstChild);
+})
