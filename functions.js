c62283dcb73fa89a3173ea49dd72c32b379d1b3f
yead0090 <yeadhossain2004@gmail.com>
2024-09-26 15:23:35 +0600

    6th commit

--- functions.js
+++ functions.js
@@ -1,7 +1,26 @@
 const btn1 = document.getElementById("btn-1");
 const btn2 = document.getElementById("btn-2");
 const btn3 = document.getElementById("btn-3");
-const balance = document.getElementById("balance")
+
+
+
+
+// common function
+function getInputFieldValueById(id) {
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
+}
+
+function getInnerTextById(id){
+    const textValue = document.getElementById(id).innerText;
+    return textValue;
+}
+
+
+
+
+
+
 const donTabBtn = document.getElementById("donate-tab-btn");
 const histTabBtn = document.getElementById("history-tab-btn");
 const donTab = document.getElementById("donation-tab");
@@ -16,9 +35,10 @@ const histTab = document.getElementById("history-tab");
 
 
 btn1.addEventListener("click", function () {
-    const input1 = Number(document.getElementById("input-1").value);
-    let result1 = Number(document.getElementById("result-1").innerText);
-    const balance = Number(document.getElementById("balance").innerText);
+    const input1 = Number(getInputFieldValueById("input-1"));
+    let result1 = Number(getInnerTextById("result-1"));
+    const balance = Number(getInnerTextById("balance"));
+
     if (input1 <= 0) {
         alert("Invalid amount")
         return;
@@ -32,15 +52,15 @@ btn1.addEventListener("click", function () {
     let newBalance = balance - input1;
     document.getElementById("balance").innerText = newBalance;
     my_modal_1.showModal()
-    
+
     return;
 })
 
 
 btn2.addEventListener("click", function () {
-    const input2 = Number(document.getElementById("input-2").value);
-    let result2 = Number(document.getElementById("result-2").innerText);
-    const balance = Number(document.getElementById("balance").innerText);
+    const input2 = Number(getInputFieldValueById("input-2"));
+    let result2 =  Number(getInnerTextById("result-2"));
+    const balance = Number(getInnerTextById("balance"));
     if (input2 <= 0) {
         alert("Invalid amount")
         return;
@@ -58,9 +78,9 @@ btn2.addEventListener("click", function () {
 
 
 btn3.addEventListener("click", function () {
-    const input3 = Number(document.getElementById("input-3").value);
-    let result3 = Number(document.getElementById("result-3").innerText);
-    const balance = Number(document.getElementById("balance").innerText);
+    const input3 = Number(getInputFieldValueById("input-3"));
+    let result3 =  Number(getInnerTextById("result-3"));
+    const balance = Number(getInnerTextById("balance"));
     if (input3 <= 0) {
         alert("Invalid amount")
         return;
@@ -78,6 +98,7 @@ btn3.addEventListener("click", function () {
 
 histTabBtn.addEventListener("click", function () {
     const histTab = document.getElementById("history-tab");
+    const donTab = document.getElementById("donation-tab");
     donTab.classList.add("hidden");
     histTab.classList.remove("hidden");
     donTabBtn.classList.remove("bg-[#B4F461]");
@@ -94,7 +115,9 @@ histTabBtn.addEventListener("click", function () {
 
 donTabBtn.addEventListener("click", function () {
     const donTab = document.getElementById("donation-tab");
+    const histTab = document.getElementById("history-tab");
     donTab.classList.remove("hidden")
+    histTab.classList.add("hidden")
     donTabBtn.classList.add("bg-[#B4F461]");
     histTabBtn.classList.remove("bg-[#B4F461]")
 
